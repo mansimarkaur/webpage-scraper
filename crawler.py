142247da31ed5326eab0bb166e8bebbc4e25f1b1
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-06-07 05:55:20 +0530

    created driver function

--- crawler.py
+++ crawler.py
@@ -32,6 +32,16 @@ def format() :
 	print soup.prettify()
 
 
+def main(option) :
+	dict = {
+		1 : image,
+		2 : hyperlinks,
+		3 : text,
+		4 : format
+	}
+	return dict.get(option, "Enter valid choice")
+
+main(option)
 
 
 
@@ -40,29 +50,6 @@ def format() :
 
 
 
-
-"""html = link.read()
-text = [k.start() for k in re.finditer('<img src="', html)]
-links = []
-for i in text :
-	string = ""
-	j = 10
-	while True :
-		if html[i+j] == '"' :
-			break
-		else :
-			string += html[i+j]
-			j += 1
-	links.append(string)
-j = 0
-for i in links :
-	image = urllib.URLopener(i)
-	image.retrieve(i, j, hook())
-	j += 1
-
-def hook(num_of_blocks, block_size, file_size) :
-	print "%s percent of the image file downloaded"%(num_of_blocks*block_size*100/file_size)
-	"""
 
 
 
