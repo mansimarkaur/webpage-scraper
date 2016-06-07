ac2c40fe80d2c0ccbfdca9755e1e4cc5591ae3e7
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-06-07 15:32:46 +0530

    menu and function updated

--- crawler.py
+++ crawler.py
@@ -11,12 +11,12 @@ print "3. Fetch text"
 print "4. Format HTML"
 option = raw_input()
 
+
 ##images
 def image() :
 	img = soup.find_all("img")
 	for i in img :
 		print i.get("src")
-
 ##links
 def hyperlinks() :
 	img = soup.find_all("a")
@@ -24,22 +24,25 @@ def hyperlinks() :
 		print i.get("href")
 
 ##text
-def text() :
+"""def text() :
 	print soup.get_text()
 
 ##formatter
 def format() :
-	print soup.prettify()
+	print soup.prettify()"""
 
+	
 
 def main(option) :
+
 	dict = {
-		1 : image,
-		2 : hyperlinks,
-		3 : text,
-		4 : format
+		"1" : image,
+		"2" : hyperlinks,
+		"3" : download,
 	}
-	return dict.get(option, "Enter valid choice")
+	func = dict.get(option, lambda : "Invalid")
+	print func
+	return func()
 
 main(option)
 
