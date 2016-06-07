f097ebb003a6d2bed67eb6ba8e2f3a18b7040ab0
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-06-07 05:36:05 +0530

    images fetched

--- crawler.py
+++ crawler.py
@@ -1,8 +1,26 @@
-import urllib
-import re
+import urllib2
+from bs4 import BeautifulSoup
 url = raw_input("Enter URL")
-link = urllib.urlopen('"'+url+'"')
-html = link.read()
+link = urllib2.urlopen(url)
+soup = BeautifulSoup(link, 'html.parser')
+
+##images
+img = soup.find_all("img")
+for i in img :
+	print i.get("src")
+
+
+
+
+
+
+
+
+
+
+
+
+"""html = link.read()
 text = [k.start() for k in re.finditer('<img src="', html)]
 links = []
 for i in text :
@@ -23,6 +41,7 @@ for i in links :
 
 def hook(num_of_blocks, block_size, file_size) :
 	print "%s percent of the image file downloaded"%(num_of_blocks*block_size*100/file_size)
+	"""
 
 
 
