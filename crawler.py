b8638e95c3cf957f3f90c57068784d3fb4e3cb33
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-06-09 22:52:55 +0530

    exit added

--- crawler.py
+++ crawler.py
@@ -1,15 +1,19 @@
 import urllib2
+import sys
 from bs4 import BeautifulSoup
 url = raw_input("Enter URL")
 link = urllib2.urlopen(url)
 soup = BeautifulSoup(link, 'html.parser')
 
-print "Choose and enter corresponding index of option"
-print "1. Fetch images"
-print "2. Fetch hyperlinks"
-print "3. Fetch text"
-print "4. Format HTML"
-option = raw_input()
+def menu():
+	print "Choose and enter corresponding index of option"
+	print "1. Fetch images"
+	print "2. Fetch hyperlinks"
+	print "3. Fetch text"
+	print "4. Format HTML"
+	print "5. Exit"
+	option = raw_input()
+	return option 
 
 
 ##images
@@ -28,10 +32,11 @@ def text() :
 	print soup.get_text().encode('UTF-8')
 
 ##formatter
-def format() :
+def formatter() :
 	print soup.prettify().encode('UTF-8')
 
-	
+def exit_scraper() :
+	sys.exit("Bye :)")
 
 def main(option) :
 
@@ -39,13 +44,21 @@ def main(option) :
 		"1" : image,
 		"2" : hyperlinks,
 		"3" : text,
-		"4" : format
+		"4" : formatter,
+		"5" : exit_scraper
 	}
-	func = dict.get(option, lambda : "Invalid")
-	print func
-	return func()
 
-main(option)
+	for i in dict :
+		if option == i :
+			func = dict.get(option, lambda option : "%s is an invalid option."%option)
+			print func
+			return func()
+	else :
+		print "Enter valid option please!"
+	main(menu())
+
+if __name__ == "__main__" :
+	main(menu())
 
 
 
