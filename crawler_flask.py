f54d2364d927a28810e53495144cd6243b64a6dc
MansimarKaur <mansimarkaur.mks@gmail.com>
2016-06-27 05:16:35 +0530

    added dowload for text

--- crawler_flask.py
+++ crawler_flask.py
@@ -7,19 +7,25 @@ from bs4 import BeautifulSoup
 
 crawler = Flask(__name__)
 
+
 @crawler.route("/")
 def main() :
 	return render_template("index.html")
 
 @crawler.route("/driver", methods = ['POST'])
 def driver() :
+	global url
 	url = request.form["inputName"] #fetches user entered URL
+	print url
 	if not(url.startswith("http://") or url.startswith("https://")) :
+		if not url.startswith("www.") :
+			url = "www." + url
 		url = "http://" + url
 	if not url.endswith("/") :
 		url = url + "/"
 	print url
 	link = urllib2.urlopen(url) #returns obj 
+	global soup
 	soup = BeautifulSoup(link, 'html.parser') #returns beautifulsoup obj
 	job = request.form["submit"]
 	dict = {
@@ -29,20 +35,35 @@ def driver() :
 		"formatter" : formatter
 		}
 	func = dict.get(job) #calls function acc to button pressed
-	return func(url, soup)
+	return func()
+
 
 #displays image URLs
-def image(url, soup) :
+def image(to_download) :
 	img = soup.find_all("img") #finds all <img>
 	if len(img) == 0 :
 		return render_template("index.html", text = ["No images to fetch"])
 	images =[]
 	for i in img :
 		images.append(url + i.get("src")) #adds src attribute value to images list
+		#if to_download :
+			#download(url, i.get("src"))
 	return render_template("images.html", text = images)
 
+def download(image) :
+	print url
+	print image
+	dir_name = url[7:]
+	print dir_name+image
+	print url+image
+	try :
+		os.stat("let")
+	except :
+		os.mkdir("let")
+	urllib.urlretrieve(url+image, "let")
+
 #displays hyperlinks
-def hyperlinks(url, soup) :
+def hyperlinks(to_download) :
 	link = soup.find_all("a") #finds all <a>
 	if len(link) == 0 :
 		return render_template("index.html", text = ["No links to fetch"])
@@ -55,20 +76,37 @@ def hyperlinks(url, soup) :
 	return render_template("links.html", text = links)
 
 #displays text after stripping html tags from src code
-def txt(url, soup) :
+def txt() :
 	t = soup.get_text()#.encode('UTF-8') 
-	print type(t)
 	return render_template("text.html", text = t)
 
 #displays formatted html src code, (not sure how it'll behave in index.html after being rendered)
-def formatter(url, soup) :
+def formatter(soup, to_download) :
 	code = soup.prettify()#.encode('UTF-8')
-	print type(code)
+	if to_download :
+		with open("dua" + ".html", "w+") as pretty :
+			pretty.write(code.encode('UTF-8'))
 	return render_template("indent.html", text = code)
 
-@crawler.route("/download") 
-def download() :
-	pass
+@crawler.route('/text', methods = ['POST'])
+def text() :
+	print "hello"
+	t = soup.get_text()#.encode('UTF-8') 
+	print "t here"
+	try :
+		print "o"
+		to_download = bool(request.form['submit'])
+		print to_download
+		file_name = request.form['name']
+		print "not"
+	except :
+		to_download = False
+		print "hee"
+	print "me"
+	if to_download :
+		with open(file_name + ".txt", "w+") as pretty :
+			pretty.write(t.encode('UTF-8'))
+	return render_template("text.html", text = t)
 
 
 if __name__ == "__main__" :
